b363f8b38ac05d04a9b0fa033866b29720504cf1
salah-so <cudasami@gmail.com>
2023-09-20 21:59:07 +0300

    calcul commit day4

---
+++ week 4/day 1/java.js
@@ -0,0 +1,25 @@
+const operator = prompt(
+    ( either +, -, * or / ): 
+ );
+ 
+ const number1 = parseFloat(prompt("Enter the first number: "));
+ const number2 = parseFloat(prompt("Enter the second number: "));
+ 
+ let result;
+ 
+ if (operator == "+") {
+  result = number1 + number2;
+ } 
+ else if (operator == "-") {
+  result = number1 - number2;
+ }
+  else if (operator == "*") {
+    result = number1 * number2;
+ } 
+ else {
+   result = number1 / number2; 
+ }
+ 
+ 
+ window.alert(" Result is" + result);
+ 
